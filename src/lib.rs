6b731237fa3305490db59f01e56de8c3f73ed5a1
zhouyi <shzy2012@gmail.com>
2022-02-18 16:39:20 +0800

    alloc

--- src/lib.rs
+++ src/lib.rs
@@ -2,15 +2,24 @@
 #![cfg_attr(test, no_main)]
 #![feature(custom_test_frameworks)]
 #![feature(abi_x86_interrupt)]
+#![feature(alloc_error_handler)] // at the top of the file
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
+extern crate alloc;
 
 use core::panic::PanicInfo;
 
+pub mod allocator;
 pub mod gdt;
 pub mod interrupts;
+pub mod memory;
 pub mod serial;
 pub mod vga_buffer;
+
+#[alloc_error_handler]
+fn alloc_error_handler(layout: alloc::alloc::Layout) -> ! {
+    panic!("allocation error: {:?}", layout)
+}
 
 pub fn init() {
     gdt::init();
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,11 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
 
+extern crate alloc;
+
 mod vga_buffer;
+use alloc::{boxed::Box, rc::Rc, vec, vec::Vec};
+use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
 
 /// This function is called on panic.
@@ -13,11 +17,12 @@ fn panic(info: &PanicInfo) -> ! {
     // loop {}
     blog_os::hlt_loop();
 }
-
+// 内部实现_start方法
+entry_point!(kernel_main);
 // static HELLO: &[u8] = b"Hello World!";
-
-#[no_mangle] // don't mangle the name of this function
-pub extern "C" fn _start() -> ! {
+// #[no_mangle] // don't mangle the name of this function
+// pub extern "C" fn _start(boot_info: &'static BootInfo) -> ! {
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
     // this function is the entry point, since the linker looks for a function
     // named `_start` by default
 
@@ -31,9 +36,78 @@ pub extern "C" fn _start() -> ! {
     // }
 
     // vga_buffer::print_something();
-    println!("Hello World{}", "!");
+    // use blog_os::memory;
+    // use blog_os::memory::BootInfoFrameAllocator;
+    use blog_os::allocator;
+    use blog_os::memory::{self, BootInfoFrameAllocator};
+    use x86_64::{structures::paging::Page, VirtAddr};
 
+    println!("Hello World{}", "!");
     blog_os::init();
+
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
+
+    // new
+    allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap initialization failed");
+
+    let heap_value = Box::new(41);
+    println!("heap_value at {:p}", heap_value);
+
+    // create a dynamically sized vector
+    let mut vec = Vec::new();
+    for i in 0..500 {
+        vec.push(i);
+    }
+    println!("vec at {:p}", vec.as_slice());
+
+    // create a reference counted vector -> will be freed when count reaches 0
+    let reference_counted = Rc::new(vec![1, 2, 3]);
+    let cloned_reference = reference_counted.clone();
+    println!(
+        "current reference count is {}",
+        Rc::strong_count(&cloned_reference)
+    );
+    core::mem::drop(reference_counted);
+    println!(
+        "reference count is {} now",
+        Rc::strong_count(&cloned_reference)
+    );
+
+    // let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    // let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    // // let mut frame_allocator = memory::EmptyFrameAllocator;
+    // let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
+
+    // // map an unused page
+    // let page = Page::containing_address(VirtAddr::new(0));
+    // memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
+
+    // // write the string `New!` to the screen through the new mapping
+    // let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    // unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e) };
+
+    // let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    // // new: initialize a mapper
+    // let mapper = unsafe { memory::init(phys_mem_offset) };
+
+    // let addresses = [
+    //     // the identity-mapped vga buffer page
+    //     0xb8000,
+    //     // some code page
+    //     0x201008,
+    //     // some stack page
+    //     0x0100_0020_1a10,
+    //     // virtual address mapped to physical address 0
+    //     boot_info.physical_memory_offset,
+    // ];
+
+    // for &address in &addresses {
+    //     let virt = VirtAddr::new(address);
+    //     let phys = mapper.translate_addr(virt);
+    //     println!("{:?} -> {:?}", virt, phys);
+    // }
 
     // fn stack_overflow() {
     //     stack_overflow(); // for each recursion, the return address is pushed
@@ -68,12 +142,12 @@ pub extern "C" fn _start() -> ! {
     // }
     // println!("write worked");
 
-    use x86_64::registers::control::Cr3;
-    let (level_4_page_table, _) = Cr3::read();
-    println!(
-        "Level 4 page table at: {:?}",
-        level_4_page_table.start_address()
-    );
+    // use x86_64::registers::control::Cr3;
+    // let (level_4_page_table, _) = Cr3::read();
+    // println!(
+    //     "Level 4 page table at: {:?}",
+    //     level_4_page_table.start_address()
+    // );
 
     println!("It did not crash!");
 
